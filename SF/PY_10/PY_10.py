96456eeb75c030b28161f49dcd0f7c34935a2732
oresh247 <oresh247@gmail.com>
2024-03-17 22:55:44 +0300

    module PY_11

--- SF/PY_10/PY_10.py
+++ SF/PY_10/PY_10.py
@@ -1,5 +1,5 @@
 import pandas as pd
-
+import numpy as np
 
 
 # def create_medications(names, counts):
@@ -40,3 +40,25 @@ import pandas as pd
 
 # print(get_profit(year = 2013, df = create_companyDF([612, 516, 329, 158], [136,163,250,361], [2017,2018,2019,2020])))
 
+# data = pd.DataFrame([[0,1], [1, 0], [1, 1]], columns=['А', 'B'])
+# print(data.shape)
+
+# melb_data = pd.read_csv('https://raw.githubusercontent.com/esabunor/MLWorkspace/master/melb_data.csv')
+melb_data = pd.read_csv('C:/Users/aoreshkin.IT-ONE/VSProject/SF_DS_PYTHON/SF/PY_10/data/melb_data.csv', sep=',')
+
+# melb_data['Car'] = melb_data['Car'].fillna(0)
+# melb_data['Bedroom'] = melb_data['Bedroom'].fillna(0)
+# melb_data['Bathroom'] = melb_data['Bathroom'].fillna(0)
+# melb_data['Propertycount'] = melb_data['Propertycount'].fillna(0)
+# melb_data['YearBuilt'] = melb_data['YearBuilt'].fillna(0)
+
+melb_data['Car'] = melb_data['Car'].astype('int64')
+melb_data['Bedroom'] = melb_data['Bedroom'].astype('int64')
+melb_data['Bathroom'] = melb_data['Bathroom'].astype('int64')
+melb_data['Propertycount'] = melb_data['Propertycount'].astype('int64')
+melb_data['YearBuilt'] = melb_data['YearBuilt'].astype('int64')
+melb_data.info()
+
+BuildingArea_median = melb_data['BuildingArea'].median() 
+BuildingArea_mean =  melb_data['BuildingArea'].mean()
+print(int(round((abs(BuildingArea_median - BuildingArea_mean)/BuildingArea_mean)*100,0)))
---
+++ SF/PY_11/PY_11.py
@@ -0,0 +1,28 @@
+import pandas as pd
+import numpy as np
+
+
+# customer_df = pd.DataFrame({
+#             'number': [0, 1, 2, 3, 4],
+#             'cust_id': [128, 1201, 9832, 4392, 7472],
+#             'cust_age': [13, 21, 19, 21, 60],
+#             'cust_sale': [0, 0, 0.2, 0.15, 0.3],
+#             'cust_year_birth': [2008, 2000, 2002, 2000, 1961],
+#             'cust_order': [1400, 14142, 900, 1240, 8430]
+#         })
+
+
+# def delete_columns(df, col=[]):
+#     set1 = set(col)
+#     if set1.issubset(df.columns ):
+#         new_df = df.drop(col, axis=1)
+#         return new_df
+#     else:
+#         return None
+
+# new_df = delete_columns(customer_df, col=['number','cust_yeadr_birth'])
+# print(new_df )
+
+
+
+
