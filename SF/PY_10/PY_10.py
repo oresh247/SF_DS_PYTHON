afc260f1df6f14be951eab15499a196c83696689
oresh247 <oresh247@gmail.com>
2024-04-21 00:01:52 +0300

    py 16

--- SF/PY_10/PY_10.py
+++ SF/PY_10/PY_10.py
@@ -3,7 +3,7 @@ import numpy as np
 
 
 # def create_medications(names, counts):
-#     medications_series = pd.Series(data=counts, index=names) 
+#     medications_series = pd.Series(data=counts, index=names)
 #     return medications_series
 
 # def get_percent(medications, name):
@@ -19,18 +19,16 @@ import numpy as np
 # print(get_percent(medications, "chlorhexidine")) #37.5
 
 
-
-
 # def create_companyDF(income, expenses, years):
 #     company_df = pd.DataFrame({
 #         'Income': income,
 #         'Expenses': expenses,
 #     }, index = years)
 #     return company_df
-    
+
 
 # def get_profit(df, year):
-#     if year in df.index: 
+#     if year in df.index:
 #         income = df.loc[year, 'Income']
 #         expenses = df.loc[year, 'Expenses']
 #         return (income - expenses)
@@ -39,12 +37,11 @@ import numpy as np
 
 
 # print(get_profit(year = 2013, df = create_companyDF([612, 516, 329, 158], [136,163,250,361], [2017,2018,2019,2020])))
-
 # data = pd.DataFrame([[0,1], [1, 0], [1, 1]], columns=['А', 'B'])
 # print(data.shape)
-
 # melb_data = pd.read_csv('https://raw.githubusercontent.com/esabunor/MLWorkspace/master/melb_data.csv')
-melb_data = pd.read_csv('C:/Users/aoreshkin.IT-ONE/VSProject/SF_DS_PYTHON/SF/PY_10/data/melb_data.csv', sep=',')
+melb_data = pd.read_csv(
+    'C:/Users/aoreshkin.IT-ONE/VSProject/SF_DS_PYTHON/SF/PY_10/data/melb_data.csv', sep=',')
 
 # melb_data['Car'] = melb_data['Car'].fillna(0)
 # melb_data['Bedroom'] = melb_data['Bedroom'].fillna(0)
@@ -59,6 +56,6 @@ melb_data['Propertycount'] = melb_data['Propertycount'].astype('int64')
 melb_data['YearBuilt'] = melb_data['YearBuilt'].astype('int64')
 melb_data.info()
 
-BuildingArea_median = melb_data['BuildingArea'].median() 
-BuildingArea_mean =  melb_data['BuildingArea'].mean()
-print(int(round((abs(BuildingArea_median - BuildingArea_mean)/BuildingArea_mean)*100,0)))
+BuildingArea_median = melb_data['BuildingArea'].median()
+BuildingArea_mean = melb_data['BuildingArea'].mean()
+print(int(round((abs(BuildingArea_median - BuildingArea_mean)/BuildingArea_mean)*100, 0)))
